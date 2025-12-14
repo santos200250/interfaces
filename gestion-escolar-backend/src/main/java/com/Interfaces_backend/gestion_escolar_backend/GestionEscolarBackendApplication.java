ad5d16c24f19649855cbe48740f9f0da65663586
Aldair <feyalegria.evin@gmail.com>
2025-12-14 00:01:54 -0500

    Mi primer commit: subiendo el proyecto

---
+++ gestion-escolar-backend/src/main/java/com/Interfaces_backend/gestion_escolar_backend/GestionEscolarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Interfaces_backend.gestion_escolar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEscolarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEscolarBackendApplication.class, args);
+	}
+
+}
---
+++ gestion-escolar-backend/src/test/java/com/Interfaces_backend/gestion_escolar_backend/GestionEscolarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Interfaces_backend.gestion_escolar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEscolarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
